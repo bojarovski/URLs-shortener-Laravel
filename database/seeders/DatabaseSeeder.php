2e785060427e84ada04b7052e076dc3815c2c487
Mario Bojarovski <bojarovskimario@gmail.com>
2024-06-07 13:46:40 +0200

    fix: update controller with free api, and fix controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call(UrlSeeder::class);
     }
 }
--- database/seeders/UrlSeeder.php
+++ database/seeders/UrlSeeder.php
@@ -20,7 +20,7 @@ class UrlSeeder extends Seeder
         foreach ($urls as $url) {
             Url::create([
                 'original_url' => $url,
-                'short_url' => substr(md5($url), 0, 6) // Generate a short URL hash
+                'short_url' => substr(md5($url), 0, 6)
             ]);
         }
     }
